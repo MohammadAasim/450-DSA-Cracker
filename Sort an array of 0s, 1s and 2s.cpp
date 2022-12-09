f36ca03d213bd545eead6a38b90ddf47c505a006
Aasim <86460933+MohammadAasim@users.noreply.github.com>
2022-12-09 23:48:26 +0530

    Add files via upload

--- Sort an array of 0s, 1s and 2s.cpp
+++ Sort an array of 0s, 1s and 2s.cpp
@@ -8,10 +8,28 @@ using namespace std;
 class Solution
 {
     public:
+    void swap(int &x , int &y){
+        int temp=x;
+        x=y;
+        y=temp;
+    }
     void sort012(int a[], int n)
     {
         // code here 
-        sort(a,a+n);
+        //sort(a,a+n);
+        int l = 0 , h=n-1, m = 0;
+        while(m<=h){
+            if(a[m]==0){
+                swap(a[m],a[l]);
+                l++;
+                m++;
+            }else if(a[m]==1){
+                m++;
+            }else{
+                swap(a[m],a[h]);
+                h--;
+            }            
+        }
     }
     
 };
